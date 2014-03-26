99554ac6cde0edf9d2f466f7f8d60d395408b4da
Antil00p <guldhamster.hopp@hotmail.com>
2014-03-26 13:42:52 +0100

    Added methods in IMovable

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IMovable.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IMovable.java
@@ -2,4 +2,13 @@ package com.whathappensingandalf.howdoiflythisthing;
 
 public interface IMovable {
 	
+	//getters
+	public Vector getSpeed();
+	public Vector getAcc();
+	public Point getPos();
+	
+	//setters
+	public void setSpeed();
+	public void setAcc();
+	public void setPos();
 }
