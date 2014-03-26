5a9031c62eed4d2e0df3967ff90f17ccbc68910b
Joakim "Kuxe" Thorén <joakimthoren93@gmail.com>
2014-03-26 13:55:15 +0100

    added IMoveable and IArmable update method in Gameworld

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Gameworld.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Gameworld.java
@@ -1,5 +1,7 @@
 package com.whathappensingandalf.howdoiflythisthing;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.HashMap;
 
 
@@ -18,16 +20,48 @@ import java.util.HashMap;
  *  moving an asteroid and moving a spaceship is exactly the same piece of code - instead of duplicating the code.
  */
 
-public class Gameworld {
+public class Gameworld implements PropertyChangeListener{
 	
 	/**
 	 * HashMaps containing all interfaces which objects in gameworld implements
 	 * These are later on looped through and logic is computed centrally from this class, Gameworld.
 	 */
-	private HashMap<IMoveable, Object> moveables;
+	private HashMap<IMovable, Object> moveables;
 	private HashMap<IArmable, Object> armables;
 	
 	public Gameworld(){
 		
-	}	
+	}
+	
+	/**
+	 * Moves all objects with IMoveable based on data provided from IMoveable
+	 */
+	private void moveableUpdate(){
+		for(IMovable ma : moveables.keySet()){
+			//Do whatever logic needed for moving
+		}
+	}
+	
+	/**
+	 * Fire projectiles from objects with IArmable if the data provided from IArmable says so.
+	 */
+	private void armableUpdate(){
+		for(IArmable aa : armables.keySet()){
+			//Do whatever logic needed for weaponry
+		}
+	}
+	
+	public void update(){
+		moveableUpdate();
+		armableUpdate();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void propertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
