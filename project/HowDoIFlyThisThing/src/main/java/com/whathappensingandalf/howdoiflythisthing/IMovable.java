257d50fd4896143b4c401346d60f8db8229a20e9
Antil00p <guldhamster.hopp@hotmail.com>
2014-03-26 15:57:30 +0100

    Changed Vector to Vector2f

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IMovable.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IMovable.java
@@ -9,14 +9,14 @@ public interface IMovable {
 	
 	public void move();
 	//getters
-	public Vector getSpeed();
-	public Vector getAcceleration();
-	public Vector getDirection();
-	public Point getPossition();
+	public Vector2f getSpeed();
+	public Vector2f getAcceleration();
+	public Vector2f getDirection();
+	public Point2f getPosition();
 	
 	//setters
-	public void setSpeed(Vector speed);
-	public void setAcceleration(Vector accelertion);
-	public void setDirection(Vector direction);
-	public void setPossition(Point possition);
+	public void setSpeed(Vector2f speed);
+	public void setAcceleration(Vector2f accelertion);
+	public void setDirection(Vector2f direction);
+	public void setPosition(Point2f position);
 }
