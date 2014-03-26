3f93d4f0eada081b1ae1c612101b66cd67a71b61
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-03-26 13:30:43 +0100

    Added the class SpaceShip

---
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/SpaceShip.java
@@ -0,0 +1,23 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+/**
+ *
+ * @author Martin Nilsson
+ */
+public class SpaceShip implements IArmable, IMovable{
+
+    private Weapon weapon;
+    
+    public SpaceShip(){
+        
+    }
+    
+    public Weapon getWeapon() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setWeapon() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
