31971523d1c9fc099406d58b841ccb90911a8895
Joakim "Kuxe" Thorén <joakimthoren93@gmail.com>
2014-03-26 14:04:01 +0100

    spaceship added to gameworld in main

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Main.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Main.java
@@ -10,6 +10,9 @@ public class Main {
 	public static void main(String argc[]){
 		Gameworld gameworld = new Gameworld();
 		
+		//Create a spaceship and add it to the gameworld
+		gameworld.addSpaceship();
+		
 		//Update gameworld 10 times.
 		for(int i = 0; i < 10; i++){
 			gameworld.update();
