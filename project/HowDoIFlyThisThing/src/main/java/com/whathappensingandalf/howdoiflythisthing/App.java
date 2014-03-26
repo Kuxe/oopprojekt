68fbb7091ee1777c5354398b9f73c4db5c5a3862
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-03-26 11:39:49 +0100

    Set up Maven project

---
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/App.java
@@ -0,0 +1,13 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
