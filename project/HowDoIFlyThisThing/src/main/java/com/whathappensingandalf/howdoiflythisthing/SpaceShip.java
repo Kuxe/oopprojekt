a2b9b5cf73ef7fe53320c853d1fe9a7d72023ebf
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-03-26 14:11:03 +0100

    Implemented the methods demanded from IMovable

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/SpaceShip.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/SpaceShip.java
@@ -7,10 +7,14 @@ package com.whathappensingandalf.howdoiflythisthing;
 public class SpaceShip implements IArmable, IMovable{
 
     private Weapon weapon;
+    private MoveComponent moveComponent = new MoveComponent();
+    private Thruster[] thruster = new Thruster[3];
     
     public SpaceShip(){
         
     }
+    
+    
     
     public Weapon getWeapon() {
         return this.weapon;
@@ -18,6 +22,30 @@ public class SpaceShip implements IArmable, IMovable{
 
     public void setWeapon(Weapon weapon) {
         this.weapon=weapon;
+    }
+
+    public Vector getSpeed() {
+        return this.moveComponent.getSpeed();
+    }
+
+    public Vector getAcc() {
+        return this.moveComponent.getAcc();
+    }
+
+    public Point getPos() {
+        return this.moveComponent.getPos();
+    }
+
+    public void setSpeed() {
+        this.moveComponent.setSpeed();
+    }
+
+    public void setAcc() {
+        this.moveComponent.setAcc();
+    }
+
+    public void setPos() {
+        this.moveComponent.setPos();
     }
     
 }
