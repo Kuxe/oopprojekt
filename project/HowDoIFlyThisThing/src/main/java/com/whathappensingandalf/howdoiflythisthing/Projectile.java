282896603df599b06a9c03a84f5157bfe28f42fe
Antil00p <guldhamster.hopp@hotmail.com>
2014-03-27 15:31:48 +0100

    Changed misspells in Projectile

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Projectile.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Projectile.java
@@ -46,18 +46,18 @@ public class Projectile implements IMovable{
 
 	//setters
 	public void setSpeed(Vector2f speed) {
-		mc.setSpeed(speed);
+		mC.setSpeed(speed);
 	}
 	public void setAcceleration(Vector2f acceleration) {
-		mc.setAcceleration(acceleration);
+		mC.setAcceleration(acceleration);
 		
 	}
 	public void setDirection(Vector2f direction) {
-		mc.setDirection(direction);
+		mC.setDirection(direction);
 		
 	}
 	public void setPosition(Point2f position) {
-		mc.setPosition(position)
+		mC.setPosition(position);
 		
 	}
 }
