335727ccd488c7b36ec33748c580f6284b9a40dc
Mathcar <mattec_93@hotmail.com>
2014-03-27 10:39:40 +0100

    implemented method create() in ProjectileFactory

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/ProjectileFactory.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/ProjectileFactory.java
@@ -1,5 +1,19 @@
 package com.whathappensingandalf.howdoiflythisthing.factorys;
 
-public class ProjectileFactory {
+import javax.vecmath.Point2f;
+import javax.vecmath.Vector2f;
 
+import com.whathappensingandalf.howdoiflythisthing.Projectile;
+
+
+/**
+ * 
+ * @author Mathias
+ *
+ */
+public class ProjectileFactory {
+	public static Projectile create(Point2f position, Vector2f speed, Vector2f acceleration, Vector2f direction){
+		return new Projectile(position, speed, acceleration, direction);
+		
+	}
 }
