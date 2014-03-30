a3736949250ecfedf295ffab93500a2e831619a9
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-03-30 19:15:34 +0200

    Added javadoc to IMovable and Projectile

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Projectile.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Projectile.java
@@ -18,6 +18,15 @@ public class Projectile implements IMovable{
 	//private Damage damage= new Damage();
 	private MoveComponent mC;
 	
+	
+	/**
+	 * Contructs a projectile with a specified position, velocity, acceleration and direction
+	 * 
+	 * @param position
+	 * @param velocity
+	 * @param acceleration
+	 * @param direction
+	 */
 	public Projectile(Point2f position, Vector2f velocity, Vector2f acceleration, Vector2f direction){
 		mC= new MoveComponent(position, velocity, acceleration, direction);
 		this.velocity= velocity;
@@ -27,37 +36,61 @@ public class Projectile implements IMovable{
 		
 	}
 	//methods
+	/**
+	 * {@inheritDoc}
+	 */
 	public void move(){
 		mC.move();
 	}
 	//getters
+	/**
+	 * {@inheritDoc}
+	 */
 	public Vector2f getVelocity() {
 		return velocity;
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	public Vector2f getAcceleration() {
 		return acceleration;
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	public Vector2f getDirection() {
 		return direction;
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	public Point2f getPosition() {
 		return position;
 	}
 
 	//setters
+	/**
+	 * {@inheritDoc}
+	 */	
 	public void setVelocity(Vector2f velocity) {
 		mC.setVelocity(velocity);
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setAcceleration(Vector2f acceleration) {
 		mC.setAcceleration(acceleration);
-		
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setDirection(Vector2f direction) {
 		mC.setDirection(direction);
-		
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setPosition(Point2f position) {
 		mC.setPosition(position);
-		
 	}
-}
+}//end Projectile
