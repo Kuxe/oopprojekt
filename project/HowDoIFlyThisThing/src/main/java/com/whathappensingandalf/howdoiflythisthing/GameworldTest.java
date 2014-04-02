08924d4643f2fbdff45ca9cc76ff69baa17f8c4a
Joakim "Kuxe" Thorén <joakimthoren93@gmail.com>
2014-04-02 15:21:26 +0200

    added gameworld test (unimplemented, empty shell)

---
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/GameworldTest.java
@@ -0,0 +1,50 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameworldTest {
+	
+	private Gameworld gameWorld;
+	
+	GameworldTest() {
+		gameWorld = new Gameworld();
+	}
+	
+	@Test
+	public void testGameworld() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveObjectFromHashMaps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSpaceshipPoint2f() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSpaceshipSpaceship() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProjectile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPropertyChange() {
+		fail("Not yet implemented");
+	}
+
+}
