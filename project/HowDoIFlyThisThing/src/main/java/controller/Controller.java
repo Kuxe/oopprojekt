ebfb56dc23e0995b17cc606069fc109285c96546
Mathias Carlsson <mattec_93@hotmail.com>
2014-04-03 18:57:26 +0200

    added support for renderObjects in conroller and view package

--- project/HowDoIFlyThisThing/src/main/java/controller/Controller.java
+++ project/HowDoIFlyThisThing/src/main/java/controller/Controller.java
@@ -1,11 +1,24 @@
 package controller;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.whathappensingandalf.howdoiflythisthing.Gameworld;
+import com.whathappensingandalf.howdoiflythisthing.IDrawable;
+
 import View.View;
 
 public class Controller {
-	//HowDoIFlyThisThing model;
+	
+	private Gameworld model;
 	View view;
+	
 	public Controller(View v){
 		view=v;
 	}
+	
+	public synchronized void setRenderObjects(List<IDrawable> list){
+		view.setRenderObjects(list);
+	}
+	
 }
