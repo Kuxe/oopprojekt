09d7b18eefb5ceb37f95c0795731a4c5a8e6d098
Joakim "Kuxe" Thorén <joakimthoren93@gmail.com>
2014-04-03 14:08:57 +0200

    hardcoded main, two spaceship exists and the first one fires a projectile

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Main.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Main.java
@@ -1,6 +1,7 @@
 package com.whathappensingandalf.howdoiflythisthing;
 
 import javax.vecmath.Point2f;
+import javax.vecmath.Vector2f;
 
 /**
  * 
@@ -12,11 +13,16 @@ public class Main {
 	public static void main(String argc[]){
 		Gameworld gameworld = new Gameworld();
 		
-		//Create a spaceship and add it to the gameworld
-		gameworld.addSpaceship(new Point2f(1, 1));
+		//Create two spaceships and make the first one shoot bullets on the other one
+		Spaceship spaceship1 = new Spaceship(new Point2f(0, 0), new Vector2f(0, 0));
+		Spaceship spaceship2 = new Spaceship(new Point2f(10, 10), new Vector2f(0, 0));
+		gameworld.addSpaceship(spaceship1);
+		gameworld.addSpaceship(spaceship2);
+		
+		spaceship1.fireWeapon();
 		
 		//Update gameworld 10 times.
-		for(int i = 0; i < 10; i++){
+		for(int i = 0; i < 100; i++){
 			gameworld.update();
 		}
 	}
