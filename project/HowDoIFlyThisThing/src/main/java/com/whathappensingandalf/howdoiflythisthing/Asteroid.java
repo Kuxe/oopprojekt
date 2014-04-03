dc98a54fad2b7f82c28055d0cc63e5e0157328a5
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-04-03 17:46:13 +0200

    Added the class Asteroid

---
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Asteroid.java
@@ -0,0 +1,61 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import java.awt.geom.Rectangle2D;
+import javax.vecmath.Point2f;
+
+/**
+ *
+ * @author Martin Nilsson
+ */
+public class Asteroid implements ICollidable{
+
+	
+	private CollidableComponent colliComp;
+	Point2f position;
+	double width;
+	double height;
+	
+	public Asteroid (Point2f position, double width, double height){
+		this.position = position;
+		this.width = width;
+		this.height = height;
+		this.colliComp = new CollidableComponent(position, width, height);
+	}
+	
+	public boolean collideDetection(ICollidable rhs) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public int getHeight() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public int getWidth() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public Point2f getPosition() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public Rectangle2D getBoundingBox() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public String getType() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void accept(ICollidable visitor) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void visit(Spaceship spaceship) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void visit(Projectile projectile) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
