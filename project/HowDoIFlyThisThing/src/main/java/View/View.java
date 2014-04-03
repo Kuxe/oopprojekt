cf8382060ea68522aab0493c6ba2e3941b7ddc75
Mathias Carlsson <mattec_93@hotmail.com>
2014-04-03 17:44:29 +0200

    Added class View

--- project/HowDoIFlyThisThing/src/main/java/View/View.java
+++ project/HowDoIFlyThisThing/src/main/java/View/View.java
@@ -1,5 +1,35 @@
 package View;
 
-public class View implements ApplicationListener{
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Game;
+import org.newdawn.slick.SlickException;
+
+import com.whathappensingandalf.howdoiflythisthing.*;
+
+public class View implements ApplicationListener{
+	
+	private AppGameContainer container;
+	private GameWindow game;
+	private List<ICollidable> renderObjects;
+	
+	public View(){
+		game=new GameWindow("How do i fly this thing?");
+		try{
+			container=new AppGameContainer(game);
+			container.setDisplayMode(640, 480, false);
+			container.setTargetFrameRate(60);
+			container.start();
+		}catch(SlickException ex){
+			Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+	}
+	
 }
