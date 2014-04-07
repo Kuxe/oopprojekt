bb978e75e94ff5fe1f3af7c648ae05933a13f40a
Joakim "Kuxe" Thorén <joakimthoren93@gmail.com>
2014-04-07 10:35:08 +0200

    added documentation to IDrawable

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IDrawable.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IDrawable.java
@@ -3,6 +3,14 @@ package com.whathappensingandalf.howdoiflythisthing;
 import javax.vecmath.Point2f;
 import javax.vecmath.Vector2f;
 
+/**
+ * 
+ * @author Joakim "Kuxe" Thorén
+ * 
+ * Note that all gameobjects implementing this interface MUST
+ * make position and direction cloneable (deep-clone) since list
+ * of drawables are copied to the view to avoid threading issues.
+ */
 
 public interface IDrawable extends IListable {
 	
