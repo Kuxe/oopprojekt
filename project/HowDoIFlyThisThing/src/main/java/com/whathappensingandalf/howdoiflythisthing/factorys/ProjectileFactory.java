bc3e5a43129d15a0ecbd6f2c2501cf3739ce02f6
Joakim "Kuxe" Thorén <joakimthoren93@gmail.com>
2014-04-08 13:51:23 +0200

    hardcoded spaceship to fire several bullets

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/ProjectileFactory.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/ProjectileFactory.java
@@ -12,11 +12,15 @@ import com.whathappensingandalf.howdoiflythisthing.Projectile;
  *
  */
 public class ProjectileFactory {
+	static int count = 0;
+	
 	public static Projectile create(Point2f position, Vector2f velocity, Vector2f acceleration, Vector2f direction, int width, int height){
 		return new Projectile(new Point2f(position), new Vector2f(velocity), new Vector2f(acceleration), new Vector2f(direction), width, height);
 		
 	}
 	public static Projectile create(Projectile p){
+		System.out.println(count);
+		count += 1;
 		return create(p.getPosition(), p.getVelocity(), p.getAcceleration(), p.getDirection(), p.getWidth(), p.getHeight());		
 	}
 }
--- project/HowDoIFlyThisThing/src/main/java/controller/Controller.java
+++ project/HowDoIFlyThisThing/src/main/java/controller/Controller.java
@@ -21,8 +21,8 @@ public class Controller {
 	private View view;
 	
 	//Create two spaceships and make the first one shoot bullets on the other one
-	Spaceship spaceship1 = new Spaceship(new Point2f(0, 0), new Vector2f(1, 0), 5, 5);
-	Spaceship spaceship2 = new Spaceship(new Point2f(500, 0), new Vector2f(-1, 0), 5, 5);
+	Spaceship spaceship1 = new Spaceship(new Point2f(0, 50), new Vector2f(1, 0), 50, 50);
+	Spaceship spaceship2 = new Spaceship(new Point2f(500, 0), new Vector2f(-1, 0), 50, 50);
 	
 	public Controller(){
 		model = new Gameworld();
