da9c23e60fb6bc6765e9ed07a875a88f5276d21c
Mathias Carlsson <mattec_93@hotmail.com>
2014-04-11 12:21:02 +0200

    Added constuctor to WorldBorder

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/WorldBorder.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/WorldBorder.java
@@ -7,4 +7,15 @@ package com.whathappensingandalf.howdoiflythisthing;
  */
 public class WorldBorder {
 	
+	private int worldHeight;
+	private int worldWidth;
+	private int maxWorldHeight;
+	private int maxWorldWidth;
+	
+	public WorldBorder(int height, int width){
+		worldHeight=height;
+		worldWidth=width;
+		maxWorldHeight=worldHeight;
+		maxWorldWidth=worldWidth;
+	}
 }
