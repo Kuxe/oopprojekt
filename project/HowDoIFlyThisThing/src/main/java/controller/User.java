48e0bb1d75462d3d9f925254a867f6811f42ac5d
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-04-11 11:24:10 +0200

    Made User implement KeyListener

--- project/HowDoIFlyThisThing/src/main/java/controller/User.java
+++ project/HowDoIFlyThisThing/src/main/java/controller/User.java
@@ -1,15 +1,42 @@
 package controller;
 
+import org.newdawn.slick.Input;
+import org.newdawn.slick.KeyListener;
+
 /**
  *
  * @author Martin Nilsson
  */
-public class User {
+public class User implements KeyListener{
 	
 	private IUserState state;
 	
 	public User(){
 		
+	}
+
+	public void keyPressed(int i, char c) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void keyReleased(int i, char c) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void setInput(Input input) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public boolean isAcceptingInput() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void inputEnded() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	public void inputStarted() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 	
 }
