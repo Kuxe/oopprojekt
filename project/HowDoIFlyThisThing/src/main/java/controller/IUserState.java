9d9880f22b52c05d3ad9a7bb204b5786548855c2
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-04-28 14:51:59 +0200

    Changed demands from IUserState

--- project/HowDoIFlyThisThing/src/main/java/controller/IUserState.java
+++ project/HowDoIFlyThisThing/src/main/java/controller/IUserState.java
@@ -6,8 +6,12 @@ package controller;
  */
 public interface IUserState {
 	
-	public void manageInput();
+	public void mainHold(boolean held);
 	
-	public void executeInput();
+	public void leftHold(boolean held);
+	
+	public void rightHold(boolean held);
+	
+	public void fireHold(boolean held);
 	
 }
