b341ac474e69b55130f54dd5b6ef018770f42ddf
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-02 15:35:33 +0200

    created testclass TimerTest

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/TimerTest.java
@@ -0,0 +1,34 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimerTest {
+
+	@Test
+	public void testTimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTimerInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsTimerDone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTimerLength() {
+		fail("Not yet implemented");
+	}
+
+}
