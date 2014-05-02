d48686160a4078e44b5e5e154c510430a3252195
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-02 17:04:59 +0200

    created TestCollidableComponent

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/TestCollidableComponent.java
@@ -0,0 +1,24 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCollidableComponent {
+
+	@Test
+	public void testCollidableComponentPoint2fDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollidableComponentPoint2fIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollideDetection() {
+		fail("Not yet implemented");
+	}
+
+}
