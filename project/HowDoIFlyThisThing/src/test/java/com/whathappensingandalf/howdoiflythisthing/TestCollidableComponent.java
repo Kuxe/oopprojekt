42a9c81ae8ca69ea394d9bae4a9bce15529d50ca
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-02 17:23:50 +0200

    Implemented testCollideDetection in TestCollidableComponent

--- project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/TestCollidableComponent.java
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/TestCollidableComponent.java
@@ -2,23 +2,28 @@ package com.whathappensingandalf.howdoiflythisthing;
 
 import static org.junit.Assert.*;
 
+import javax.vecmath.Point2f;
+import javax.vecmath.Vector2f;
+
 import org.junit.Test;
 
 public class TestCollidableComponent {
 
 	@Test
 	public void testCollidableComponentPoint2fDouble() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testCollidableComponentPoint2fIntInt() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testCollideDetection() {
-		fail("Not yet implemented");
+		CollidableComponent c1=new CollidableComponent(new Point2f(10,10),1);
+		ICollidable c2=new Spaceship(new Point2f(10,10),new Vector2f(0,0),2,2);
+		assertTrue(c1.collideDetection(c2));
 	}
 
 }
