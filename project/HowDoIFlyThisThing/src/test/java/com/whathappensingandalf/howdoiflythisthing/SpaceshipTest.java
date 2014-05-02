df17926df0a4c900a69491e88998e3fe63b98bd0
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-02 11:46:58 +0200

    Fixed test 30 in SpaceshipTest

--- project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/SpaceshipTest.java
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/SpaceshipTest.java
@@ -175,12 +175,14 @@ public class SpaceshipTest implements PropertyChangeListener{
 	public void testCalculateThrust() {
 		s.setAcceleration(new Vector2f(0,70));
 		s.setVelocity(new Vector2f(0,10));
+		s.setRotAcceleration(5);
 		Timestep t = new Timestep();
 		t.start();
 		t.end();
 		t.calculateDeltatime();
 		s.calculateThrust(t);
-		assertTrue(s.getAcceleration().equals(new Vector2f(0,0))&&s.getVelocity().equals(new Vector2f(0,10)));
+		
+		assertTrue(s.getAcceleration().equals(new Vector2f(0,0))&&s.getVelocity().equals(new Vector2f(0,10))&&s.getRotAcceleration()==0);
 	}
 
 	@Test
@@ -229,6 +231,13 @@ public class SpaceshipTest implements PropertyChangeListener{
 
 	@Test
 	public void testActivateMainThruste() {
+		Spaceship ship = new Spaceship(new Point2f(100, 100), new Vector2f(1,0), 10, 10);
+		ship.activateMainThruste();Timestep t = new Timestep();
+		t.start();
+		t.end();
+		t.calculateDeltatime();
+		ship.calculateThrust(t);
+		assertTrue(!(ship.getAcceleration().equals(new Vector2f(0,0))));
 	}
 
 	@Test
