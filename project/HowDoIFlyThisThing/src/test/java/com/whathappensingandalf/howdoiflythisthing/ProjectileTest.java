8db7516fb42f0bb249cbb2777b5ff65ac2f0d984
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-05-02 13:36:52 +0200

    testMove() in ProjectileTest done

--- project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ProjectileTest.java
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ProjectileTest.java
@@ -2,168 +2,166 @@ package com.whathappensingandalf.howdoiflythisthing;
 
 import static org.junit.Assert.*;
 
+import javax.vecmath.Point2f;
+import javax.vecmath.Vector2f;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ProjectileTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+public class ProjectileTest{
+	
+	private Vector2f vec= new Vector2f(5, 5);
+	private Projectile pro= new Projectile(new Point2f(0, 0), vec, vec, new Vector2f(0, 1), 3, 3);
+//											position, velocity, acceleration, direction, width, height
+	
+//	assertTrue(true);
 
 	@Test
-	public void testProjectilePoint2fVector2fVector2fVector2fIntInt() {
-		fail("Not yet implemented");
-	}
+	public void testProjectilePoint2fVector2fVector2fVector2fIntInt(){
+//		should constructor be tested?
+		assertTrue(true);
+		}
 
 	@Test
 	public void testProjectileProjectile() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
-	public void testMove() {
-		fail("Not yet implemented");
+	public void testMove(){
+		Timestep timestep= new Timestep();
+		timestep.start();
+		timestep.end();
+		timestep.calculateDeltatime();
+		pro.move(timestep);
+		assertTrue(pro.getPosition().x > 0);
 	}
 
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetVelocity() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetAcceleration() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetDirection() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetPosition() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testSetVelocity() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testSetAcceleration() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testSetDirection() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testSetPosition() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetRotVelocity() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetRotAcceleration() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testSetRotVelocity() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testSetRotAcceleration() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetDamage() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetHeight() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetWidth() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetType() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testCollideDetection() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testAccept() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testVisitSpaceship() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testVisitProjectile() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testVisitAsteroid() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testAddPropertyChangeListener() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testRemovePropertyChangeListener() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testClone() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
-
-}
+}//end ProjectileTest
