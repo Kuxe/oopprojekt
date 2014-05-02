689573e2b195bd102e82990e3f4d5b65608ef4fe
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-02 17:18:50 +0200

    Created a test class for TypeWrapper

---
+++ project/HowDoIFlyThisThing/src/test/java/utils/TypeWrapperTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class TypeWrapperTest {
+	
+	public TypeWrapperTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testSetValue() {
+	}
+
+	@Test
+	public void testGetValue() {
+	}
+
+	@Test
+	public void testEquals() {
+	}
+	
+}
