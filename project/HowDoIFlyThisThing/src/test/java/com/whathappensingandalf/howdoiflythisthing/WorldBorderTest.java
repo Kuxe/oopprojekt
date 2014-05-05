101195e7da632639382edc3d5115dfe011270c44
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-05 10:12:24 +0200

    Created worldBorderTest class

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/WorldBorderTest.java
@@ -0,0 +1,14 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorldBorderTest {
+
+	@Test
+	public void testWorldBorder() {
+		fail("Not yet implemented");
+	}
+
+}
