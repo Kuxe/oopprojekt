dfd9bde1478b11bbeac7467576496ace87763f95
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-05 09:30:45 +0200

    added methodes in Spaceship to return whether the indiviual thrusters are active.

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Spaceship.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Spaceship.java
@@ -341,4 +341,14 @@ public class Spaceship implements IMovable, IThrustable, ICollidable, IGameObjec
 					this.width==other.width;
 		}
 	}
+	
+	public boolean isMainThusterActive(){
+		return thrusterComponent.isMainThusterActive();
+	}
+	public boolean isLeftThusterActive(){
+		return thrusterComponent.isLeftThusterActive();
+	}
+	public boolean isRightThusterActive(){
+		return thrusterComponent.isRightThusterActive();
+	}
 }
