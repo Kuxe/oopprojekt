d32f97007f497be8707f1ac31397201b3f552b4a
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-05 10:25:32 +0200

    Created testclass AsteroidTest

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/AsteroidTest.java
@@ -0,0 +1,74 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AsteroidTest {
+
+	@Test
+	public void testAsteroidPoint2fIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAsteroidAsteroid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollideDetection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccept() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVisitSpaceship() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVisitProjectile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVisitAsteroid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+}
