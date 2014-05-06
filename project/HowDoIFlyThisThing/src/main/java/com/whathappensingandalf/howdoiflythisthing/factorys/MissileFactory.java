eb5714b2b57550ebbd3bfe8391862f35f3031685
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-06 17:17:59 +0200

    corected the first mehod create in MissileFactory

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/MissileFactory.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/MissileFactory.java
@@ -7,7 +7,7 @@ import com.whathappensingandalf.howdoiflythisthing.Missile;
 
 public class MissileFactory {
 	public static Missile create(Point2f position, Vector2f velocity, Vector2f acceleration, Vector2f direction, int width, int height){
-		return new Missile(position, velocity, acceleration, direction, width, height);
+		return new Missile(new Point2f(position), new Vector2f(velocity), new Vector2f(acceleration), new Vector2f(direction), width, height);
 	}
 	
 	public static Missile create(Missile missile){
