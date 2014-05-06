0e104a416e253cd8d50fe40b34eda506720a912f
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-06 19:57:51 +0200

    Implemented accept in WeaponPickup

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/WeaponPickup.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/WeaponPickup.java
@@ -57,7 +57,7 @@ public class WeaponPickup implements IPickup{
 	}
 
 	public void accept(ICollidable visitor) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		visitor.visit(this);
 	}
 
 	public void visit(Spaceship spaceship) {
