caf14a8252487a8e6e75447b289173aefe82e262
Joakim Kuxe Thorén <joakimthoren93@gmail.com>
2014-05-07 01:00:45 +0200

    removed obsolete lock code from ViewThread

--- project/HowDoIFlyThisThing/src/main/java/View/ViewThread.java
+++ project/HowDoIFlyThisThing/src/main/java/View/ViewThread.java
@@ -3,19 +3,12 @@ package View;
 public class ViewThread extends Thread implements Runnable{
 	public View v;
 	
-	boolean isReady = false;
-	Object lock;
-	
 	public ViewThread(Object lock){
-		this.lock = lock;
 		v=new View("How do i fly this?", lock);
 	}
 	
 	
 	public void run(){
-		synchronized(lock) {
-			lock.notifyAll();
-		}
 		v.start();
 	}
 	
