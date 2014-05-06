749977e38e9006ad2742c07d353784dd9e6f1f0c
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-06 15:30:18 +0200

    Added a method to IPickup

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IPickup.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/IPickup.java
@@ -5,4 +5,5 @@ package com.whathappensingandalf.howdoiflythisthing;
  * @author Martin Nilsson
  */
 public interface IPickup extends ICollidable, IGameObject, IDrawable, Cloneable{
+	public void affectMe(Spaceship spaceship);
 }
