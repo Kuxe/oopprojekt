cd9708e9103911ab9f29aff262eb5f033e9b7702
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-06 17:10:29 +0200

    Created a test for HealthPickup

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/HealthPickupTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.whathappensingandalf.howdoiflythisthing;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class HealthPickupTest {
+	
+	public HealthPickupTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testGetHealth() {
+	}
+
+	@Test
+	public void testAffectMe() {
+	}
+
+	@Test
+	public void testRemove() {
+	}
+
+	@Test
+	public void testCollideDetection() {
+	}
+
+	@Test
+	public void testGetHeight() {
+	}
+
+	@Test
+	public void testGetWidth() {
+	}
+
+	@Test
+	public void testGetPosition() {
+	}
+
+	@Test
+	public void testGetType() {
+	}
+
+	@Test
+	public void testAccept() {
+	}
+
+	@Test
+	public void testVisit_Spaceship() {
+	}
+
+	@Test
+	public void testVisit_IProjectile() {
+	}
+
+	@Test
+	public void testVisit_Asteroid() {
+	}
+
+	@Test
+	public void testVisit_IPickup() {
+	}
+
+	@Test
+	public void testGetDirection() {
+	}
+
+	@Test
+	public void testClone() {
+	}
+
+	@Test
+	public void testAddPropertyChangeListener() {
+	}
+
+	@Test
+	public void testRemovePropertyChangeListener() {
+	}
+	
+}
