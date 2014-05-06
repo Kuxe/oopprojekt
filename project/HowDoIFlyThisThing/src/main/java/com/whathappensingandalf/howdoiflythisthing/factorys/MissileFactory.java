d09f3c085e6ee57b19bc1fe99418b3eb8bb2b28a
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-06 17:11:28 +0200

    created first mehod create in MissileFactory

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/MissileFactory.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/factorys/MissileFactory.java
@@ -1,5 +1,10 @@
 package com.whathappensingandalf.howdoiflythisthing.factorys;
 
-public class MissileFactory {
+import javax.vecmath.Point2f;
+import javax.vecmath.Vector2f;
 
+public class MissileFactory {
+	public static Missile create(Point2f position, Vector2f velocity, Vector2f acceleration, Vector2f direction, int width, int height){
+		
+	}
 }
