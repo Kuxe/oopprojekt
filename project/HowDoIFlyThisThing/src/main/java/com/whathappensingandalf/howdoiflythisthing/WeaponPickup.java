0362f0436b85958ae1de31e390be7b60132b2a44
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-06 19:51:36 +0200

    Implemented collision detection in WeaponPickup

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/WeaponPickup.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/WeaponPickup.java
@@ -37,7 +37,7 @@ public class WeaponPickup implements IPickup{
 	}
 	
 	public boolean collideDetection(ICollidable rhs) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return this.collidableComp.collideDetection(rhs);
 	}
 
 	public int getHeight() {
