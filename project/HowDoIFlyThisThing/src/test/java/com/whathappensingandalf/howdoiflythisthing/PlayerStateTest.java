40b02df4818a5b8714e1cdb6d65243b495719c32
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-05-06 16:10:46 +0200

    Method testSetSpaceship in PlayerStateTest done

--- project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/PlayerStateTest.java
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/PlayerStateTest.java
@@ -2,6 +2,9 @@ package com.whathappensingandalf.howdoiflythisthing;
 
 import static org.junit.Assert.*;
 
+import javax.vecmath.Point2f;
+import javax.vecmath.Vector2f;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,6 +12,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PlayerStateTest {
+	
+	private PlayerState playerState= new PlayerState();
+	private boolean held= false;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,37 +34,44 @@ public class PlayerStateTest {
 
 	@Test
 	public void testSetSpaceship() {
-		fail("Not yet implemented");
+		Point2f pos= new Point2f(1, 2);
+		Vector2f dir= new Vector2f(3, 4);
+		int w= 50;
+		int h= 40;
+		Spaceship s1= new Spaceship(pos, dir, w, h);
+		playerState.setSpaceship(s1);
+		assertTrue(playerState.getSpaceship().getPosition().equals(pos) && playerState.getSpaceship().getDirection().equals(dir) &&
+				playerState.getSpaceship().getWidth()== w && playerState.getSpaceship().getHeight()== h);
 	}
 
 	@Test
 	public void testMainHold() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testLeftHold() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testRightHold() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testFireHold() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetSpaceshipPosition() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testGetSpaceship() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 }
