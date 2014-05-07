fa916b2de5414ab81227ae041340ac2cf7c8ec26
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-07 17:04:40 +0200

    Created class ThrusterTest

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ThrusterTest.java
@@ -0,0 +1,44 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ThrusterTest {
+
+	@Test
+	public void testThruster() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActivate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeactivate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMagnitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAcceleration() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRotationAcceleration() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsThrusterActive() {
+		fail("Not yet implemented");
+	}
+
+}
