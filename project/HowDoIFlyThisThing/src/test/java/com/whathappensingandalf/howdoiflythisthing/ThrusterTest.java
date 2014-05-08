3b722598c9d7836deadac8268165b2e35129e01d
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-08 10:43:55 +0200

    test method setMagnitude in Thruster

--- project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ThrusterTest.java
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ThrusterTest.java
@@ -31,7 +31,9 @@ public class ThrusterTest {
 
 	@Test
 	public void testSetMagnitude() {
-		fail("Not yet implemented");
+		Thruster t=new Thruster(10,20,50,1);
+		t.setMagnitude(100);
+		assertTrue(t.getDirection().length()==100);
 	}
 
 	@Test
