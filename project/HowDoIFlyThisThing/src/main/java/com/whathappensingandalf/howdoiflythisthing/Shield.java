72d2f70fabb1af0b3ac11c4ae96366402ad578df
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-09 11:06:55 +0200

    implemented methodes in Shield

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Shield.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Shield.java
@@ -3,6 +3,7 @@ package com.whathappensingandalf.howdoiflythisthing;
 public class Shield {
 	
 	private int shield;
+	private int maxShield;
 	
 	public Shield(){
 		this(3);
@@ -10,6 +11,36 @@ public class Shield {
 	
 	public Shield(int shield){
 		this.shield=shield;
+		this.maxShield=shield;
+	}
+	
+	/**
+	 * 
+	 * @param damage
+	 * @return the remaining damage after damageing shield
+	 */
+	public int hurt(int damage){
+		if(damage<shield){
+			shield=shield-damage;
+			return 0;
+		}else{
+			damage=damage-shield;
+			shield=0;
+			return damage;	
+		}
+	}
+	
+	public void recharge(){
+		if(shield<maxShield){
+			shield++;
+		}
+	}
+	
+	public void setShield(int s){
+		shield=s;
+		if(shield>maxShield){
+			shield=maxShield;
+		}
 	}
 	
 }
