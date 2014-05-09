afdb5716f7654e6e9cba3e144b10d7fbeb015db1
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-09 10:22:32 +0200

    added consructors in Shield

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Shield.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Shield.java
@@ -1,5 +1,15 @@
 package com.whathappensingandalf.howdoiflythisthing;
 
 public class Shield {
-
+	
+	private int shield;
+	
+	public Shield(){
+		this(3);
+	}
+	
+	public Shield(int shield){
+		this.shield=shield;
+	}
+	
 }
