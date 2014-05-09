c00f9640c071722a4732cbd313d6aa01d8d2df39
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-09 10:22:21 +0200

    added consructors in Hull

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Hull.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Hull.java
@@ -1,5 +1,15 @@
 package com.whathappensingandalf.howdoiflythisthing;
 
 public class Hull {
-
+	
+	private int hull;
+	
+	public Hull(){
+		this(10);
+	}
+	
+	public Hull(int hull){
+		this.hull=hull;
+	}
+	
 }
