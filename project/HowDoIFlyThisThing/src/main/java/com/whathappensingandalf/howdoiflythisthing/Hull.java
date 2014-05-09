49f2d19e1dba48dbbef9249321063d4e4dc46f80
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-09 11:06:45 +0200

    implemented methodes in Hull

--- project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Hull.java
+++ project/HowDoIFlyThisThing/src/main/java/com/whathappensingandalf/howdoiflythisthing/Hull.java
@@ -3,6 +3,7 @@ package com.whathappensingandalf.howdoiflythisthing;
 public class Hull {
 	
 	private int hull;
+	private int maxHull;
 	
 	public Hull(){
 		this(10);
@@ -10,6 +11,23 @@ public class Hull {
 	
 	public Hull(int hull){
 		this.hull=hull;
+		this.maxHull=hull;
+	}
+	
+	public boolean hurt(int damage){
+		hull=hull-damage;
+		if(hull<1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setHull(int h){
+		hull=h;
+		if(hull<maxHull){
+			hull=maxHull;
+		}
 	}
 	
 }
