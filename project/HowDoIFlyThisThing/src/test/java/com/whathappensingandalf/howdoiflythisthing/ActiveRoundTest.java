ee9bcd34dee9c2545dda7f33f140d03e0d2f1779
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-05-22 16:19:15 +0200

    Added ActiveRoundTest

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ActiveRoundTest.java
@@ -0,0 +1,53 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ActiveRoundTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddUser() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testRemoveUser() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetState() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testAddListener() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetStatus() {
+		assertTrue(true);
+	}
+}
