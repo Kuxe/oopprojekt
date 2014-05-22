8baf0490c07dc000b54593e548a2b9509b08ccc2
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-05-22 16:14:16 +0200

    Changes in AsteroidTest

--- project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/AsteroidTest.java
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/AsteroidTest.java
@@ -16,24 +16,24 @@ public class AsteroidTest implements PropertyChangeListener{
 	@Test
 	public void testAsteroidPoint2fIntInt() {
 		Asteroid testAsteroid=new Asteroid(new Point2f(30,40),10,20);
-		assertTrue(testAsteroid.getPosition().x==30 &&
-				testAsteroid.getPosition().y==40 &&
-				testAsteroid.getWidth()==10 &&
-				testAsteroid.getHeight()==20);
+		assertTrue(testAsteroid.getPosition().x==30);
+		assertTrue(testAsteroid.getPosition().y==40);
+		assertTrue(testAsteroid.getWidth()==10);
+		assertTrue(testAsteroid.getHeight()==20);
 	}
 
 	@Test
 	public void testAsteroidAsteroid() {
 		Asteroid testAsteroid=new Asteroid(new Point2f(30,40),10,20);
 		Asteroid testAsteroid2=new Asteroid(testAsteroid);
-		assertTrue(testAsteroid2.getPosition().x==30 &&
-				testAsteroid2.getPosition().y==40 &&
-				testAsteroid2.getWidth()==10 &&
-				testAsteroid2.getHeight()==20 &&
-				testAsteroid2.getPosition().x==testAsteroid.getPosition().x &&
-				testAsteroid2.getPosition().y==testAsteroid.getPosition().y &&
-				testAsteroid2.getWidth()==testAsteroid.getWidth() &&
-				testAsteroid2.getHeight()==testAsteroid.getHeight());
+		assertTrue(testAsteroid2.getPosition().x==30);
+		assertTrue(testAsteroid2.getPosition().y==40);
+		assertTrue(testAsteroid2.getWidth()==10);
+		assertTrue(testAsteroid2.getHeight()==20);
+		assertTrue(testAsteroid2.getPosition().x==testAsteroid.getPosition().x);
+		assertTrue(testAsteroid2.getPosition().y==testAsteroid.getPosition().y);
+		assertTrue(testAsteroid2.getWidth()==testAsteroid.getWidth());
+		assertTrue(testAsteroid2.getHeight()==testAsteroid.getHeight());
 	}
 
 	@Test
@@ -61,7 +61,7 @@ public class AsteroidTest implements PropertyChangeListener{
 		Asteroid testAsteroid=new Asteroid(testPoint,10,20);
 		if(Math.abs(testAsteroid.getPosition().x - testPoint.x) < 0.0000001 &&
 				Math.abs(testAsteroid.getPosition().y - testPoint.y) < 0.0000001){
-		assertTrue(true);
+			assertTrue(true);
 		}
 	}
 
@@ -98,24 +98,23 @@ public class AsteroidTest implements PropertyChangeListener{
 	@Test
 	public void testGetDirection() {
 		Asteroid testAsteroid=new Asteroid(new Point2f(30,40),10,20);
-		assertTrue(testAsteroid.getDirection().x==0 &&
-				testAsteroid.getDirection().y==1);
+		assertTrue(testAsteroid.getDirection().x==0);
+		assertTrue(testAsteroid.getDirection().y==1);
 	}
 
 	@Test
 	public void testClone() {
 		Asteroid testAsteroid=new Asteroid(new Point2f(30,40),10,20);
 		Asteroid testAsteroidClone=testAsteroid.clone();
-		assertTrue(
-				testAsteroid!=testAsteroidClone &&
-				testAsteroid.getDirection()!=testAsteroidClone.getDirection() &&
-				testAsteroid.getDirection().x==testAsteroidClone.getDirection().x &&
-				testAsteroid.getDirection().y==testAsteroidClone.getDirection().y &&
-				testAsteroid.getPosition()!=testAsteroidClone.getPosition() &&
-				testAsteroid.getPosition().x==testAsteroidClone.getPosition().x &&
-				testAsteroid.getPosition().y==testAsteroidClone.getPosition().y &&
-				testAsteroid.getWidth()==testAsteroidClone.getWidth() &&
-				testAsteroid.getHeight()==testAsteroidClone.getHeight());
+		assertTrue(testAsteroid!=testAsteroidClone);
+		assertTrue(testAsteroid.getDirection()!=testAsteroidClone.getDirection());
+		assertTrue(testAsteroid.getDirection().x==testAsteroidClone.getDirection().x);
+		assertTrue(testAsteroid.getDirection().y==testAsteroidClone.getDirection().y);
+		assertTrue(testAsteroid.getPosition()!=testAsteroidClone.getPosition());
+		assertTrue(testAsteroid.getPosition().x==testAsteroidClone.getPosition().x);
+		assertTrue(testAsteroid.getPosition().y==testAsteroidClone.getPosition().y);
+		assertTrue(testAsteroid.getWidth()==testAsteroidClone.getWidth());
+		assertTrue(testAsteroid.getHeight()==testAsteroidClone.getHeight());
 	}
 
 	public void propertyChange(PropertyChangeEvent evt) {
