c926a17b4683e67ae6894d2e91c77b8c68e6fbd4
Mathias Carlsson <mattec_93@hotmail.com>
2014-05-22 19:09:47 +0200

    created testclass ClientStateTest

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/ClientStateTest.java
@@ -0,0 +1,79 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientStateTest {
+
+	@Test
+	public void testClientState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDrawableData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSpaceshipPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCleanup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListOfSounds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetShield() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCountdown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetModelStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWorldBorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPropertyChangeListener() {
+		fail("Not yet implemented");
+	}
+
+}
