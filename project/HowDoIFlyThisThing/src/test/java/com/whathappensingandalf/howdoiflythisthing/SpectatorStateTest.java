4162c89a4e3933e632cd9a90a6306746716897f4
Kjell Martin Nilsson <kjell.martin.nilsson@gmail.com>
2014-05-22 17:22:08 +0200

    Created test for SpectatorStateTest

---
+++ project/HowDoIFlyThisThing/src/test/java/com/whathappensingandalf/howdoiflythisthing/SpectatorStateTest.java
@@ -0,0 +1,79 @@
+package com.whathappensingandalf.howdoiflythisthing;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class SpectatorStateTest {
+	
+	public SpectatorStateTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testMainHold() {
+	}
+
+	@Test
+	public void testLeftHold() {
+	}
+
+	@Test
+	public void testRightHold() {
+	}
+
+	@Test
+	public void testFireHold() {
+	}
+
+	@Test
+	public void testSetCameraPoint() {
+	}
+
+	@Test
+	public void testGetSpaceshipPosition() {
+	}
+
+	@Test
+	public void testGetSpaceship() {
+	}
+
+	@Test
+	public void testSuicide() {
+	}
+
+	@Test
+	public void testGetState() {
+	}
+
+	@Test
+	public void testGetHull() {
+	}
+
+	@Test
+	public void testGetShield() {
+	}
+	
+}
